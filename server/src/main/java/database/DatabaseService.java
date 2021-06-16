f989a788110c2b4512f0f910f4db4531e85b5833
ulyanovskk <mr1english4@gmail.com>
2021-06-17 02:21:50 +0300

    КРУТО

--- server/src/main/java/database/DatabaseService.java
+++ server/src/main/java/database/DatabaseService.java
@@ -5,10 +5,7 @@ import objects.Difficulty;
 import objects.Discipline;
 import objects.LabWork;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
 
 public class DatabaseService {
@@ -17,15 +14,13 @@ public class DatabaseService {
             "  (id, labname, coordinate_x, coordinate_y, minimal_point, difficulty, discipline, self_study_hours, creation_date, labwork_id) VALUES " +
             " (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
     private final static String CLEAR_SQL = "TRUNCATE TABLE labworks";
-    private final static String REMOVE_BY_ID_SQL = "DELETE FROM labworks WHERE (id = ?) AND (author = ?)";
+    private final static String REMOVE_BY_ID_SQL = "DELETE FROM labworks WHERE (id = ?))";
     private final static String ADD_USER_SQL = "INSERT INTO users (user, password) VALUES (?, ?);";
-    private final static String UPDATE_SQL = "UPDATE labworks set WHERE id = ? and author = ? " +
+    private final static String UPDATE_SQL = "UPDATE labworks set WHERE labwork_id = ? " +
             "labname = ? , coordinate_x = ? , coordinate_y = ? , minimal_point = ? , difficulty = ? , discipline = ? " +
             ", self_study_hours = ? , creation_date = ?, labwork_id = ? ;";
-    private final static String AVG_MINIMAL_POINT_SQL = "SELECT AVG(minimal_point) FROM labworks WHERE author = ?";
-    private final static String REMOVE_LOWER_SQL = "DELETE FROM labworks WHERE labwork_id < ? and author = ?";
-    private final static String REMOVE_BY_DISCIPLINE_SQL = "DELETE FROM labworks WHERE discipline = ? and self_study_hours = ? and author = ?";
-    private final static String DESCENDING_DIFFICULTY_SQL = "SELECT difficulty FROM labworks WHERE author = ? ORDER BY difficulty";
+    private final static String REMOVE_LOWER_SQL = "DELETE FROM labworks WHERE labwork_id < ?";
+    private final static String REMOVE_BY_DISCIPLINE_SQL = "DELETE FROM labworks WHERE discipline = ? and self_study_hours = ?";
     private final static String GET_SQL = "SELECT * FROM labworks";
 
     public DatabaseService() {
@@ -49,6 +44,61 @@ public class DatabaseService {
         }
         return collection;
     }
+
+    public void update(int id, LabWork labWork) throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL);
+        preparedStatement.setInt(1, id);
+        preparedStatement.setString(2, labWork.getName());
+        preparedStatement.setDouble(3, labWork.getCoordinates().getX());
+        preparedStatement.setDouble(4, labWork.getCoordinates().getY());
+        preparedStatement.setLong(5, labWork.getMinimalPoint());
+        preparedStatement.setString(6, labWork.getDifficulty().toString());
+        preparedStatement.setString(7, labWork.getDiscipline().getName());
+        preparedStatement.setLong(8, labWork.getDiscipline().getSelfStudyHours());
+        preparedStatement.setString(9, labWork.getCreationDate().toString());
+        preparedStatement.executeUpdate();
+    }
+    public void remove(String key) throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_BY_ID_SQL);
+        preparedStatement.setInt(1, Integer.parseInt(key));
+        preparedStatement.execute();
+    }
+    public void clear() throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(CLEAR_SQL);
+        preparedStatement.execute();
+    }
+    public void removeAllLower(LabWork labWork) throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_LOWER_SQL);
+        preparedStatement.setLong(1, labWork.getId());
+        preparedStatement.execute();
+    }
+    public void removeByDiscipline(Discipline discipline) throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_BY_DISCIPLINE_SQL);
+        preparedStatement.setString(1, discipline.getName());
+        preparedStatement.setLong(2, discipline.getSelfStudyHours());
+        preparedStatement.execute();
+
+    }
+    public void insert(String key, LabWork labWork) throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(INSERT_LABWORK_SQL);
+        preparedStatement.setInt(1, Integer.parseInt(key));
+        preparedStatement.setString(2, labWork.getName());
+        preparedStatement.setDouble(3, labWork.getCoordinates().getX());
+        preparedStatement.setDouble(4, labWork.getCoordinates().getY());
+        preparedStatement.setLong(5, labWork.getMinimalPoint());
+        preparedStatement.setString(6, labWork.getDifficulty().toString());
+        preparedStatement.setString(7, labWork.getDiscipline().getName());
+        preparedStatement.setLong(8, labWork.getDiscipline().getSelfStudyHours());
+        preparedStatement.setString(9, labWork.getCreationDate().toString());
+        preparedStatement.execute();
+    }
+
 
     public static DatabaseService getInstance() {
         return ds;
--- server/src/main/java/logic/Editor.java
+++ server/src/main/java/logic/Editor.java
@@ -19,6 +19,7 @@ import java.util.stream.Stream;
 
 public class Editor {
     HashMap<String, LabWork> collection;
+    DatabaseService databaseService;
 
     public Editor() {
         readCollectionFromDatabase();
@@ -27,6 +28,7 @@ public class Editor {
     private void readCollectionFromDatabase() {
         synchronized (DatabaseService.getInstance()) {
             try {
+                databaseService = DatabaseService.getInstance();
                 collection = DatabaseService.getInstance().getCollection();
             } catch (SQLException exception) {
                 exception.printStackTrace();
@@ -47,21 +49,23 @@ public class Editor {
     }
 
     public void removeElementByKey(String key) {
-        collection.remove(key);
+        try {
+            databaseService.remove(key);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void clear() {
-        collection.clear();
+        try {
+            databaseService.clear();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
-    public void removeAllLowerByLabwork(LabWork labWork) {
-//        ArrayList<String> toDeleteKeys = new ArrayList<>();
-//        for (String key: collection.keySet()) {
-//            if (collection.get(key).compareTo(labWork) < 0) toDeleteKeys.add(key);
-//        }
-//        for (String key3: toDeleteKeys) collection.remove(key3);
-        //collection.keySet().stream().filter(key->collection.get(key).compareTo(labWork) < 0).forEach(key->collection.remove(key));
-        collection.keySet().stream().filter(key->collection.get(key).compareTo(labWork) < 0).collect(Collectors.toList()).forEach(key->collection.remove(key));
+    public void removeAllLowerByLabwork(LabWork labWork) throws SQLException {
+        databaseService.removeAllLower(labWork);
     }
 
     public String getAverageMinimalPoint() {
@@ -83,45 +87,32 @@ public class Editor {
     }
 
     public String removeByDiscipline(Discipline discipline) {
-        Stream<String> stream = collection.keySet().stream().filter(
-                s -> collection.get(s).getDiscipline().equals(discipline));
-        Optional<String> optional = stream.findFirst();
-        if (optional.isPresent()) {
-            collection.remove(optional.get());
-            return "Successfully removed element.";
-        };
-        return "No matches.";
+        try {
+            databaseService.removeByDiscipline(discipline);
+            return "REMOVED";
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return "Tried to remove.";
     }
 
     public void update(int id, LabWork labWork) {
-//        boolean notFound = true;
-//        for (String key: collection.keySet()) {
-//            LabWork labWork1 = collection.get(key);
-//            if (labWork1.getId() == id) {
-//                labWork1.copyFromLabwork(labWork);
-//                notFound = false;
-//            }
-//        }
-//        if (notFound) throw new NoSuchElementException();
-       Optional<String> optional = collection.keySet().stream().filter(s->collection.get(s).getId() == id).findFirst();
-       if (!optional.isPresent()) throw new NoSuchElementException();
-       collection.replace(optional.get(), labWork);
-
+        try {
+            databaseService.update(id, labWork);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void insert(String key, LabWork labwork) {
-        collection.put(key, labwork);
+        try {
+            databaseService.insert(key, labwork);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
+    
+    public void save() {
 
-    public void save(String filename) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
-        File file = Paths.get(filename).toFile();
-        file.setWritable(true);
-        writer.writeValue(new FileOutputStream(file), collection);
-    }
-
-    public void removeIfLower(String key, LabWork labWork) {
-        if (collection.get(key).compareTo(labWork) < 0) collection.remove(key);
     }
 }
