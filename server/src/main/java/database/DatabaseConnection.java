f473f1da56a537312930c3241249284085a0d8fb
ulyanovskk <mr1english4@gmail.com>
2021-06-17 00:28:48 +0300

    Help me

--- server/src/main/java/database/DatabaseConnection.java
+++ server/src/main/java/database/DatabaseConnection.java
@@ -4,4 +4,5 @@ public class DatabaseConnection {
     private String login = "s309553";
     private String password = "znx749";
     private String url = "jdbc:postgresql://localhost:21212/studs";
+
 }
